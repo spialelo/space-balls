e1b4809c61063ccee615f3bcbee3998377579271
Nicole <ndphillips@gmail.com>
2017-05-15 04:51:36 +0000

    Completed addFish and loadSamples methods.

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,36 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+    
+    constructor(){
+        super();
+        this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
+        //getInitialState
+        this.state = {
+            fishes:{},
+            order: {}
+        };
+    }
+    
+    addFish(fish){
+        //update state
+        const fishes = {...this.state.fishes}; //using spread to make a copy of current state
+        const timestamp = Date.now();
+        fishes[`fish-${timestamp}`] = fish;
+        //tell react that state has been updated
+        this.setState({fishes});
+    }
+    
+    loadSamples(){
+        this.setState({
+            fishes: sampleFishes
+        });
+    }
+    
     render(){
     {/*One method that is always needed on a component: render*/} 
         return(
@@ -13,7 +41,7 @@ class App extends React.Component {
                     <Header tagline ="Fresh Seafood Market"/>
                 </div>
             <Order/>
-            <Inventory/>
+            <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
             </div>
             
             
---
+++ src/components/Fish.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import {render} from 'react-dom';
+
+class Fish extends React.Component {
+    render(){
+        return(
+            <li className="menu-fish">
+            
+            </li>
+            )
+    }
+}
+
+export default Fish;
