6278944e7268e4800e38eb9504e391b38f367ec9
Nicole <ndphillips@gmail.com>
2017-06-01 03:56:26 +0000

    Working on styling using React CSSTransitionGroup

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { formatPrice } from '../helpers';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Order extends React.Component{
     
@@ -45,14 +46,20 @@ class Order extends React.Component{
         return(
             <div className="order-wrap">
                 <h2>Your Order</h2>
-                <ul className="order">
+                <CSSTransitionGroup 
+                className="order"
+                component="ul"
+                transitionName = "order"
+                transitionEnterTimeout = {500}
+                transitionLeaveTimeout = {500}
+                >
                     {orderIds.map(this.renderOrder)}
                     <li className="total">
                         <strong>Total:</strong>
                         {formatPrice(total)}
                         
                     </li>
-                </ul>
+                </CSSTransitionGroup>
             </div>
             )
     }
